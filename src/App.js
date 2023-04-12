1f362a889ae22f454a8c4afefef44135286b8010
Evan Donica <cevandonica@gmail.com>
2023-04-12 12:45:44 -0500

    Updated routers using createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import { 
+    createBrowserRouter,
+    Route,
+    Link,
+    NavLink, 
+    createRoutesFromElements,
+    RouterProvider} from "react-router-dom";
 
 import Home from './pages/Home';
 import Create from './pages/create'
+
 import './index.css';
+
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path = "/" element = {<RootLayout/>}>
+         <Route index element= {<Home />} />
+         <Route path="create" element= {<Create />} />
+    </Route>
+    )
+)
 
 const App = () =>{
     return(
-        <BrowserRouter>
-        <header>
-            <nav>
-                <h1>Visual Schedule Builder</h1>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/create">Create</NavLink>
-            </nav>
-        </header>
-            <main>
-                <Routes>
-                    <Route index element= {<Home />}/>
-                    <Route path="/create" element= {<Create />}/>
-                </Routes>
-            </main>
-        </BrowserRouter>
+            <RouterProvider router={router}/>
 )};
 
 export default App
---
+++ src/layouts/RootLayout.js
@@ -0,0 +1,18 @@
+import { NavLink, Outlet} from "react-router-dom"
+
+export default function RootLayout(){
+    return(
+        <div className="root-layout">
+            <header>
+                <nav>
+                    <h1>Visual Schedule Builder</h1>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/create">Create</NavLink>
+                </nav>
+            </header>
+            <main>
+                <Outlet />
+            </main>
+         </div>
+    )
+}
