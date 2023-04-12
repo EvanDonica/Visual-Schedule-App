6b5ed7f84651ec1b582b3c35090e30aed8f00d52
Evan Donica <cevandonica@gmail.com>
2023-04-12 13:59:35 -0500

    Added new fields to the create page and added CSS

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,14 +1,25 @@
 export default function Create(){
     return(
         <div className="create">
-            <h2>Visual Schedule Form</h2>
+            <h2>Create a New Schedule</h2>
             <form>
-              <label>Step Title: </label>  
-              <input 
+            <label>Step Title: </label>  
+            <input 
               type="text"
               required
-              />
+            />
+            <label>Description: </label>  
+            <textarea
+                required
+            ></textarea>
+            <label>Add-On</label>
+            <select>
+                <option value = "none">none</option>
+                <option value = "timer">timer</option>
+                <option value = "tokens">tokens</option>
+            </select>
             </form>
+            <button>Add Step</button>
         </div>
     )
 }
