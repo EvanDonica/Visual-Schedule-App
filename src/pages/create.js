633856e6a03266a4eb75aa6ee1c2f46ba2a4b95f
Evan Donica <cevandonica@gmail.com>
2023-04-13 17:48:28 -0500

    Added json server and did some work on the create form

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,4 +1,29 @@
+import { useEffect, useState } from "react"
+
 export default function Create(){
+    const [steps, setSteps] = useState(null);
+
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [addOn, setAddOn] = useState('')
+    const [image, setImage] = useState()
+
+    const onImageChange = (event) => {
+        if (event.target.files && event.target.files[0]) {
+          setImage(URL.createObjectURL(event.target.files[0]));
+        }
+       }
+
+    useEffect(()=>{
+        fetch('http://localhost:8000/step')
+        .then(res=> {
+            return res.json();
+        })
+        .then(data =>{
+
+        })
+    }, []);
+
     return(
         <div className="create">
             <h2>Create a New Schedule</h2>
@@ -7,19 +32,34 @@ export default function Create(){
             <input 
               type="text"
               required
+              value = {title}
+              onChange = {(event)=> setTitle(event.target.value)}
             />
             <label>Description: </label>  
             <textarea
                 required
+                value = {description}
+                onChange = {(event)=> setDescription(event.target.value)}
             ></textarea>
             <label>Add-On</label>
-            <select>
-                <option value = "none">none</option>
+            <select
+            value = {addOn}
+            onChange = {(event)=> setAddOn(event.target.value)}
+            >
+                <option value = "">none</option>
                 <option value = "timer">timer</option>
                 <option value = "tokens">tokens</option>
             </select>
+            <input 
+            type="file" 
+            onChange={onImageChange} 
+            className="filetype" />
             </form>
             <button>Add Step</button>
+            <p>{title}</p>
+            <p>{description}</p>
+            <p>{addOn}</p>
+            <img alt="preview image" src={image}/>
         </div>
     )
 }
